bf3ec3482c671f653e038ca93fee06ff8c712538
Panway <wooodypan@foxmail.com>
2023-02-27 12:44:54 +0800

    -

---
+++ PandaNote/Base/Controller/PPDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PPDetailViewController.swift
+//  PandaNote
+//
+//  Created by topcheer on 2023/2/26.
+//  Copyright © 2023 WeirdPan. All rights reserved.
+//
+
+import Foundation
+
+class DetailViewController: UIViewController {
+    var filePathStr: String = ""
+    var fileID = ""
+    var downloadURL = ""
+    
+    
+    let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    let viewControllers = [PPMarkdownViewController(), PPWebViewController()]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addChild(pageViewController)
+        view.addSubview(pageViewController.view)
+        pageViewController.view.frame = view.bounds
+        
+        guard let markdown = viewControllers[0] as? PPMarkdownViewController else {return};
+        markdown.filePathStr = filePathStr
+        markdown.fileID = fileID
+        markdown.downloadURL = downloadURL
+        pageViewController.setViewControllers([markdown], direction: .forward, animated: true, completion: nil)
+        pageViewController.didMove(toParent: self)
+    }
+}
--- PandaNote/Tool/PPAppConfig.swift
+++ PandaNote/Tool/PPAppConfig.swift
@@ -43,13 +43,13 @@ class PPAppConfig: NSObject {
     }
     
     func initDataBase() {
-        let dbModel = PPPriceDBModel()
-        let sqliteManager = PPSQLiteManager(delegate: dbModel)
-//        let sqliteManager = PPSQLiteManager.shared
-        sqliteManager.createDB()
-        let sql = "create table if not exists pp_price(id integer primary key autoincrement,code varchar(20) not null,price varchar(20) default 0,name varchar(50), whole_price varchar(20) ,remark varchar(20),category varchar(20) )"
-        
-        sqliteManager.operation(process: sql, value: [])
+//        let dbModel = PPPriceDBModel()
+//        let sqliteManager = PPSQLiteManager(delegate: dbModel)
+////        let sqliteManager = PPSQLiteManager.shared
+//        sqliteManager.createDB()
+//        let sql = "create table if not exists pp_price(id integer primary key autoincrement,code varchar(20) not null,price varchar(20) default 0,name varchar(50), whole_price varchar(20) ,remark varchar(20),category varchar(20) )"
+//        
+//        sqliteManager.operation(process: sql, value: [])
     }
     func getItem(_ key:String) -> String {
         return config[key] ?? ""
