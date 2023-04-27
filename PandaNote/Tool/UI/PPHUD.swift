9c67ec10e765cfe66b9cb30c1a37e79631a194b9
Panway <wooodypan@foxmail.com>
2023-04-27 12:29:35 +0800

    -

--- PandaNote/Tool/UI/PPHUD.swift
+++ PandaNote/Tool/UI/PPHUD.swift
@@ -22,9 +22,13 @@ class PPHUD: NSObject {
     var waitHUb:MBProgressHUD?
     let deleteBGView = UIView()
     let annularView = MBRoundProgressView()
+    let barProgressView = MBBarProgressView()
     var canceled = false
     var hud = MBProgressHUD(view: UIApplication.shared.keyWindow!)
-
+//    let tipsLB = UILabel()
+    var displayMsgCount = 0
+    var lastY: CGFloat = 0.0
+    
     lazy var revokeBtn : UIButton = {
         let button = UIButton(type: .custom)
         button.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
@@ -43,35 +47,30 @@ class PPHUD: NSObject {
         label.frame = CGRect(x: 55, y: 5, width: 88, height: 35)
         return label
     }()
+    
+    
     //MARK: 顶部提示信息框
     class func showHUDFromTop(_ message:String, isError:Bool?=false) -> Void {
         if(message.length < 1) {
             return
         }
         guard let keyWindow = UIApplication.shared.keyWindow else { return }
-        var tag = 9998
+        let tag = 9998
         let width = max(320, keyWindow.frame.width)
-        var hudH = CGFloat(20.0)
-        var hudLeft = 80.0
-        if let lastView = keyWindow.viewWithTag(tag) as? UILabel {
-            tag = 9999
-            lastView.frame.origin.x = 20.0
-            hudLeft = lastView.frame.origin.x + lastView.frame.size.width + 20
-            
-//            lastView.text = (lastView.text ?? "") + " " + message
-//            cancelHide = true
-//            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-//                lastView.removeFromSuperview()
-//                cancelHide = false
-//            }
-        }
+        var top = CGFloat(20.0)
+        let hudLeft = 80.0
+
         if #available(iOS 11.0, *) {
-            hudH = keyWindow.safeAreaInsets.top
+            top = keyWindow.safeAreaInsets.top
         }
-        let aLB = UILabel(frame: CGRect(x: hudLeft,
-                                        y: -40.0,
-                                        width: hudH * CGFloat(message.length),
-                                        height: hudH))
+        if shared.lastY == 0 {
+            shared.lastY = top
+        }
+        let aLB = UILabel() //PPHUD.shared.tipsLB
+        aLB.frame = CGRect(x: hudLeft,
+                           y: 0.0,
+                           width: top * CGFloat(message.length),
+                           height: top)
 //        aLB.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         if let isError = isError, isError {//isError存在且值为true
             aLB.backgroundColor = UIColor(hexRGBValue: 0xf75356)
@@ -79,8 +78,8 @@ class PPHUD: NSObject {
         else {
             aLB.backgroundColor = UIColor(red:0.27, green:0.68, blue:0.49, alpha:1.00)//VUE绿
         }
-        aLB.textColor = UIColor.white
-        aLB.textAlignment = NSTextAlignment.center
+        aLB.textColor = .white
+        aLB.textAlignment = .center
         aLB.numberOfLines = 3
         aLB.font = UIFont.systemFont(ofSize: 12)
         aLB.text = "\(message)"
@@ -88,20 +87,32 @@ class PPHUD: NSObject {
         aLB.layer.masksToBounds = true
         aLB.layer.cornerRadius = 8
         aLB.alpha = 0.7
-        UIApplication.shared.keyWindow?.addSubview(aLB)
+        keyWindow.addSubview(aLB)
         let size = aLB.getSize(constrainedWidth: width)
 
-        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 25, options: [.curveEaseInOut,.beginFromCurrentState], animations: {
-            aLB.frame = CGRect(x: hudLeft, y: hudH*2, width: size.width+30, height: hudH)
-
+        UIView.animate(withDuration: 0.8,
+                       delay: 0,
+                       usingSpringWithDamping: 0.9,
+                       initialSpringVelocity: 25,
+                       options: [.curveEaseOut,.beginFromCurrentState],
+                       animations: {
+            var y2 = shared.lastY + size.height+10 + 8.0
+//            debugPrint(" \(shared.lastY) + \(size.height+10 + 8.0) = \(y2) [\(shared.displayMsgCount)]")
+            aLB.frame = CGRect(x: hudLeft, y: y2, width: size.width+10, height: size.height+10)
+            shared.lastY = y2
         }) { (complete) in
 
         }
-
+        shared.displayMsgCount += 1
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-//            if cancelHide == false {
-                aLB.removeFromSuperview()
-//            }
+            aLB.removeFromSuperview()
+            shared.displayMsgCount -= 1
+//            debugPrint("msg:\(shared.displayMsgCount)")
+            if shared.displayMsgCount == 0 {
+                shared.lastY = top
+//                debugPrint("shared.lastY:\(shared.lastY)")
+
+            }
         }
 //        hud.mode = .text
 //        hud.detailsLabel.text = message
@@ -160,7 +171,7 @@ class PPHUD: NSObject {
         view.addSubview(waitHUb!)
         waitHUb?.show(animated: true)
     }
-    
+    */
     func removeWaitView()  {
         waitHUb?.hide(animated: true, afterDelay: 0)
         waitHUb = nil
@@ -192,15 +203,15 @@ class PPHUD: NSObject {
     func showDelayTaskHUD(completion: (() -> Void)? = nil) {
         let bottomPadding: CGFloat
         guard let keyWindow = UIApplication.shared.keyWindow else { return }
-        let width = max(320, keyWindow.frame.width)
+//        let width = max(320, keyWindow.frame.width)
         if #available(iOS 11.0, *) {
-            bottomPadding = UIApplication.shared.keyWindow?.safeAreaInsets.bottom ?? 0
+            bottomPadding = keyWindow.safeAreaInsets.bottom
         } else {
             bottomPadding = 0
         }
         deleteBGView.frame = CGRect(x: 20, y: keyWindow.frame.size.height - 45 - bottomPadding, width: keyWindow.frame.size.width-40, height: 45)
         deleteBGView.backgroundColor = UIColor(white: 0, alpha: 0.8)
-        UIApplication.shared.keyWindow?.addSubview(deleteBGView)
+        keyWindow.addSubview(deleteBGView)
         deleteBGView.addSubview(annularView)
         annularView.frame = CGRect(x: 20, y: 10, width: 25, height: 25)
         
